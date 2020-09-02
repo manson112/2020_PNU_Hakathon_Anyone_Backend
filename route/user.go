a6e1988af46cd801e2476f86219774fd22fcfbea
Kwanwoo Kim <rlarhksdn777@naver.com>
2020-09-02 23:06:59 +0900

    bookmark returns with more data

--- route/user.go
+++ route/user.go
@@ -15,13 +15,21 @@ type UserReq struct {
 
 // Bookmark :: Selected bookmark data from database
 type Bookmark struct {
-	ID         string `json:"id"`
-	CategoryID string `json:"category_id"`
-	Image      string `json:"image"`
-	Category   string `json:"category"`
-	StoreName  string `json:"store_name"`
-	Address    string `json:"address"`
-	CreatedAt  string `json:"created_at"`
+	ID          int    `json:"id"`
+	CategoryID  string `json:"category_id"`
+	Category    string `json:"category"`
+	StoreName   string `json:"name"`
+	TotalSeat   int    `json:"total_seat"`
+	CurrentSeat int    `json:"current_seat"`
+	Address     string `json:"address"`
+	ImageURL    string `json:"image"`
+	Lat         string `json:"lat"`
+	Lng         string `json:"lng"`
+	Noise       string `json:"noise"`
+	Cleanliness string `json:"cleanliness"`
+	Kindness    string `json:"kindness"`
+	Wifi        string `json:"wifi"`
+	CreatedAt   string `json:"created_at"`
 }
 
 // SearchHistory :: Selected search history data from database
@@ -39,10 +47,26 @@ func GetBookmarks(c *gin.Context) {
 		c.JSON(300, model.Get300Response(""))
 	}
 
-	query := "SELECT distinct B.id, B.category_id, B.image, C.name cateogry, B.name store_name, B.address, A.created_at " +
+	query := "SELECT distinct " +
+		"B.id, B.category_id, B.image,  " +
+		"C.name category, B.name store_name,  " +
+		"B.address, B.total_seat, B.current_seat, " +
+		"B.lat, B.lng, " +
+		"IFNULL(D.noise, 0) noise,  " +
+		"IFNULL(D.cleanliness, 0) cleanliness,  " +
+		"IFNULL(D.kindness, 0) kindness,  " +
+		"IFNULL(D.wifi, 0) wifi,  " +
+		"A.created_at " +
 		"FROM bookmark A " +
 		"LEFT JOIN store_info B ON A.store_id = B.id " +
 		"LEFT JOIN category C ON B.category_id = C.id " +
+		"LEFT JOIN (SELECT store_id, " +
+		"AVG(noise) noise, " +
+		"AVG(cleanliness) cleanliness, " +
+		"AVG(kindness) kindness, " +
+		"AVG(wifi) wifi " +
+		"FROM review " +
+		"GROUP BY store_id ) D ON A.store_id = D.store_id " +
 		"WHERE A.user_id= " + userReq.UserID + " " +
 		"ORDER BY A.created_at DESC;"
 
@@ -57,9 +81,21 @@ func GetBookmarks(c *gin.Context) {
 	var bookmarks []Bookmark
 	for results.Next() {
 		var bookmark Bookmark
-		err = results.Scan(&bookmark.ID, &bookmark.CategoryID, &bookmark.Image,
-			&bookmark.Category, &bookmark.StoreName,
-			&bookmark.Address, &bookmark.CreatedAt)
+		err = results.Scan(&bookmark.ID,
+			&bookmark.CategoryID,
+			&bookmark.ImageURL,
+			&bookmark.Category,
+			&bookmark.StoreName,
+			&bookmark.Address,
+			&bookmark.TotalSeat,
+			&bookmark.CurrentSeat,
+			&bookmark.Lat,
+			&bookmark.Lng,
+			&bookmark.Noise,
+			&bookmark.Cleanliness,
+			&bookmark.Kindness,
+			&bookmark.Wifi,
+			&bookmark.CreatedAt)
 		if err != nil {
 			log.Println(err)
 			log.Println("Cannot get data")
@@ -67,7 +103,6 @@ func GetBookmarks(c *gin.Context) {
 			return
 		}
 		bookmarks = append(bookmarks, bookmark)
-		log.Println(bookmark.Image)
 	}
 	c.JSON(200, model.Get200Response(bookmarks))
 }
